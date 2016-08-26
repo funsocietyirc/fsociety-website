7f38d2f745a4d82fd042b91e2130065daf3e4263
Dave Richer <dave@develops.io>
2016-08-26 04:40:09 -0400

    clickable episodes

--- resources/views/episodes.blade.php
+++ resources/views/episodes.blade.php
@@ -4,16 +4,18 @@
         <div class="uk-vertical-align-middle uk-text-center">
             <div class="episodes">
                 <ul class="uk-grid" data-uk-grid-margin>
-                    @foreach($episodes as $episode)
+                   @foreach($episodes as $episode)
                         <li class="uk-animation-fade uk-width-small-1-3 uk-width-medium-1-3 uk-width-large-1-5">
-                            @if($episode['imageMedium'] )
-                                <img src="{!! $episode['imageMedium'] !!}" alt="">
-                            @else
-                                <img src="{!! asset('images/episodes/placeholdermed.jpg') !!}" alt="">
-                            @endif
-                            <p>
-                                {!! $episode['name'] !!}
-                            </p>
+                            <div class="episode-item" data-episode-link="">
+                                @if($episode['imageMedium'] )
+                                    <img src="{!! $episode['imageMedium'] !!}" alt="">
+                                @else
+                                    <img src="{!! asset('images/episodes/placeholdermed.jpg') !!}" alt="">
+                                @endif
+                                <p>
+                                    {!! $episode['name'] !!}
+                                </p>
+                            </div>
                         </li>
                     @endforeach
                 </ul>
@@ -22,4 +24,20 @@
     </div>
 @endsection
 @section('scripts')
+    <script>
+        $(function() {
+            $('.episode-item').each(function(index, value) {
+                var self = $(this);
+                var link = self.data('episode-link');
+                self.click(function () {
+                   window.location = link;
+                });
+                self.hover(function() {
+                    self.children().toggleClass('hovered')
+                }, function() {
+                    self.children().toggleClass('hovered')
+                });
+            });
+        });
+    </script>
 @endsection
