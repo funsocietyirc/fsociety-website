5938315d0d1c13cd678f9b39e4177761796b7c23
Dave Richer <dave@develops.io>
2016-08-26 07:52:49 -0400

    show episode

--- resources/views/episodes/show.blade.php
+++ resources/views/episodes/show.blade.php
@@ -3,12 +3,15 @@
     <div class="full-height show-episode">
         <div class="uk-grid">
             <div class="uk-width-1-1">
-                <div class="uk-block uk-block-medium uk-cover-background" style="background-image:url({!! $episode['imageOriginal'] !!});">
+                <div class="uk-block uk-block-medium uk-cover-background" style="@if($episode['imageOriginal'])background-image:url({!! $episode['imageOriginal'] !!}); @else background-image:url({!! asset('images/episodes/banner.png') !!});@endif">
                         <h1 class="uk-text-truncate uk-text-center">{!! $episode['name'] !!}</h1>
                 </div>
                 <dl class="uk-description-list-horizontal">
                     <dt>Season</dt>
-                    <dd>{!! $episode['season_id'] !!}</dd>
+                    <dd>
+                        <a href="{{route('season',$episode['season_id'])}}">
+                        {!! $episode['season_id'] !!}</dd>
+                        </a>
                     <dt>Episode</dt>
                     <dd>{!! $episode['number'] !!}</dd>
                     <dt>Air Date</dt>
