eef433902610dbf864734a3f6346a9c1ecf01c5e
Dave Richer <dave@develops.io>
2016-08-29 07:49:50 -0400

    test

--- resources/views/arg/index.blade.php
+++ resources/views/arg/index.blade.php
@@ -2,7 +2,7 @@
 @section('content')
     <div class="arg-layout">
         <div class="uk-grid">
-            <div class="arg-items uk-width-1-1">
+            <div class="uk-width-1-1">
                 <header class="uk-block  uk-cover-background">
                     <h1 class="uk-text-truncate uk-text-center">
                         Alternate Reality Links
@@ -22,28 +22,28 @@
                         </ul>
                     </div>
                 </div>
-                <div class="uk-grid uk-grid-large" data-uk-grid>
-                    @foreach($results as $result)
-                    <article class="arg-item uk-container-center uk-width-large-1-3 uk-width-medium-1-2">
-                        <div class="uk-block uk-cover-background" @if(File::exists(public_path('images/arg/tiles/'.$result->id.'.png'))) style="background:url('{!! asset('images/arg/tiles/' . $result->id .'.png') !!}')"@endif>
-                            <div class="uk-container" style="background:rgba(0,0,0,0.8);padding-top:10px">
-                                <h3><a href="{!! $result->url !!}" target="_blank">{!! title_case($result->name) !!}</a></h3>
-                                <hr>
-                                <dl class="uk-description-list-horizontal">
-                                    <dt>Created By</dt>
-                                    <dd>{!! $result->creator->name !!}</dd>
-                                    <dt>On</dt>
-                                    <dd>{!! $result->created_at->diffForHumans() !!}</dd>
-                                    <dt>Last Modified</dt>
-                                    <dd>{!! $result->updated_at->diffForHumans() !!}</dd>
-                                </dl>
-                                <p>
-                                    {!! $result->description !!}
-                                </p>
-                            </div>
-                        </div>
-                    </article>
-                    @endforeach
+                <div class="arg-items">
+                    <div class="uk-grid uk-grid-large" data-uk-grid>
+                        @foreach($results as $result)
+                            <article class="arg-item uk-width-large-1-3 uk-width-medium-1-2">
+                                <div class="uk-block uk-cover-background" @if(File::exists(public_path('images/arg/tiles/'.$result->id.'.png'))) style="background:url('{!! asset('images/arg/tiles/' . $result->id .'.png') !!}') "@endif>
+                                    <div class="uk-container" style="padding-top:10px">
+                                        <h3><a href="{!! $result->url !!}" target="_blank">{!! title_case($result->name) !!}</a></h3>
+                                        <hr>
+                                        <dl class="uk-description-list-horizontal">
+                                            <dt>Created By</dt>
+                                            <dd>{!! $result->creator->name !!}</dd>
+                                            <dt>Last Modified</dt>
+                                            <dd>{!! $result->updated_at->diffForHumans() !!}</dd>
+                                        </dl>
+                                        <p>
+                                            {!! $result->description !!}
+                                        </p>
+                                    </div>
+                                </div>
+                            </article>
+                        @endforeach
+                    </div>
                 </div>
 
             </div>
