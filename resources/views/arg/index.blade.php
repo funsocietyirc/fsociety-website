ccbd6270f0b96dd436043fc2d7affda867d66d7c
Dave Richer <dave@develops.io>
2016-09-01 09:02:47 -0400

    layout change

--- resources/views/arg/index.blade.php
+++ resources/views/arg/index.blade.php
@@ -22,11 +22,11 @@
                     </div>
                 </div>
 
-                <div class="uk-grid uk-grid-large" data-uk-grid-margin>
+                <div class="uk-grid uk-grid-large uk-grid-match" data-uk-grid-margin>
                     @foreach($results as $result)
                         <article class="arg-item uk-width-large-1-3 uk-width-medium-1-2">
                             <div class="arg-item-content">
-                                <div class="uk-block uk-cover-backgrounder" @if(File::exists(public_path('images/arg/tiles/'.$result->id.'.png'))) style="background:url('{!! asset('images/arg/tiles/' . $result->id .'.png') !!}') "@endif>
+                                <div class="uk-block uk-cover-backgrounder uk-grid-match" @if(File::exists(public_path('images/arg/tiles/'.$result->id.'.png'))) style="background:url('{!! asset('images/arg/tiles/' . $result->id .'.png') !!}') "@endif>
                                     <div class="height-1-1 inner-block">
                                         <h3><a href="{{ $result->url }}"
                                                target="_blank">{{ title_case($result->name) }}</a></h3>
