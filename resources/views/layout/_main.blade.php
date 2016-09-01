1a0e036d915ec2c721fc8c71f1e424169d0a3f93
Dave Richer <dave@develops.io>
2016-09-01 04:57:05 -0400

    Do not use cachebusting on prod

--- resources/views/layout/_main.blade.php
+++ resources/views/layout/_main.blade.php
@@ -4,9 +4,9 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <script src="{{elixir('js/app.js')}}"></script>
-    <link rel="stylesheet" href={{elixir('css/uikit.css')}}>
-    <link rel="stylesheet" href={{elixir('css/app.css')}}>
+    <script src="{{config('app.env') === 'local' ? elixir('js/app.js') : asset('js/app.js') }}"></script>
+    <link rel="stylesheet" href={{config('app.env') === 'local' ? elixir('css/uikit.css') : asset('css/uikit.css')}}>
+    <link rel="stylesheet" href={{config('app.env') === 'local' ? elixir('css/app.css') : asset('css/app.css')}}>
     @yield('head-style')
     <title>FSociety - @yield('title')</title>
 </head>
