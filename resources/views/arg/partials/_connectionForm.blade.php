04ef95b2fd190bb66ffa6732ce8af22244f30906
Dave Richer <dave@develops.io>
2016-09-03 20:07:19 -0400

    formatting

--- resources/views/arg/partials/_connectionForm.blade.php
+++ resources/views/arg/partials/_connectionForm.blade.php
@@ -5,7 +5,7 @@
       data-confirm="Are you sure you want to add this Connection?" data-disable-with="Saving..." action="{{route('arg.connect', $result)}}" method="post">
     {{csrf_field()}}
     <select class="uk-form-small" name="episode">
-        <option disabled selected>Add new Mention</option>
+        <option disabled selected>Add new Connection</option>
         @foreach($result->availableConnections() as $connection)
             <option value="{{$connection->id}}">{{$connection->name}}</option>
         @endforeach
