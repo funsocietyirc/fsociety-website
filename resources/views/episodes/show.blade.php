140db7bf203f6afdde4b03de44d36ae9b30a3a9f
Dave Richer <dave@develops.io>
2016-09-03 22:23:21 -0400

    Formatting

--- resources/views/episodes/show.blade.php
+++ resources/views/episodes/show.blade.php
@@ -5,26 +5,26 @@
         <div class="uk-grid">
             <div class="uk-width-1-1">
                 <div class="episode-header uk-block uk-cover-background"
-                     style="@if($episode->imageOriginal)background-image:url({!! $episode->imageOriginal !!}); @else background-image:url({!! asset('images/episodes/banner.png') !!});@endif">
+                     style="@if($episode->imageOriginal)background-image:url({{ $episode->imageOriginal }}); @else background-image:url({{ asset('images/episodes/banner.png') }});@endif">
                     <div class="uk-cover-background">
                         <div class="uk-container uk-container-center">
                             <div class="uk-block episode-info">
-                                <h1 class="uk-text-truncate uk-text-center">{!! $episode->name !!}</h1>
+                                <h1 class="uk-text-truncate uk-text-center">{{ $episode->name }}</h1>
                                 <dl class="uk-description-list-horizontal">
                                     <dt>Season</dt>
                                     <dd>
                                         <a href="{{route('season',$episode->season_id)}}">
-                                            {!! $episode->season_id !!}
+                                            {{ $episode->season_id }}
                                         </a>
                                     </dd>
                                     <dt>Episode</dt>
-                                    <dd>{!! $episode->number !!}</dd>
+                                    <dd>{{ $episode->number }}</dd>
                                     <dt>Air Date</dt>
-                                    <dd>{!! $episode->airdate !!}</dd>
+                                    <dd>{{ $episode->airdate }}</dd>
                                     <dt>Run Time</dt>
-                                    <dd>{!! $episode->runtime !!} minuets</dd>
+                                    <dd>{{ $episode->runtime }} minuets</dd>
                                     <dt>Summary</dt>
-                                    <dd>{!! $episode->summary !!}</dd>
+                                    <dd>{{ $episode->summary }}</dd>
                                 </dl>
                             </div>
                         </div>
@@ -36,9 +36,7 @@
                             <h3>Alternate Reality Links</h3>
                             <ul class="uk-subnav uk-subnav-line">
                                 @foreach($episode->connections()->get() as $connection)
-                                    <li>
-                                        <a href="{{route('arg.show',$connection->argLink->slug)}}">{{$connection->argLink->name}}</a>
-                                    </li>
+                                    <li><a href="{{route('arg.show',$connection->argLink->slug)}}">{{$connection->argLink->name}}</a></li>
                                 @endforeach
                             </ul>
                         </div>
