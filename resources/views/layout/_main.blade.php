d3fa6c436e4d3791e9a6822e50771d28fdfecdaa
Dave Richer <dave@develops.io>
2016-09-24 00:00:03 -0400

    remove Content policy

--- resources/views/layout/_main.blade.php
+++ resources/views/layout/_main.blade.php
@@ -6,7 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{!! csrf_token() !!}"/>
     <meta name="csrf-param" content="_token"/>
-    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     <script src="{{elixir('js/app.js')}}"></script>
     <link rel="stylesheet" href={{elixir('css/uikit.css')}}>
     <link rel="stylesheet" href={{elixir('css/app.css')}}>
