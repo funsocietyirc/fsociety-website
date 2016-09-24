6cf60175e9f4bafbb1cb4591007caa72e529da3e
Dave Richer <dave@develops.io>
2016-09-24 03:53:49 -0400

    gallery improvements

--- resources/assets/js/components/gallery.vue
+++ resources/assets/js/components/gallery.vue
@@ -1,39 +1,47 @@
 <template>
     <div class="gallery-container">
-        <div class="">
-            <div class="uk-grid">
-                <div class="uk-width-large-1-6">
-                    <div class="uk-panel uk-panel-header">
-                        <h3 class="uk-panel-title">0ptions</h3>
-                    </div>
+        <div id="image-modal" class="uk-modal" data-uk-modal="{center:true}" style="position:fixed" >
+            <div class="uk-modal-dialog uk-modal-dialog-lightbox  uk-modal-dialog-large uk-text-center">
+                <a href="" class="uk-modal-close uk-close uk-close-alt gallery-close-alt"></a>
+                <img v-bind:src="activeImage" alt="">
+            </div>
+        </div>
+        <div class="uk-grid">
+            <div class="uk-width-large-1-6">
+                <div id="options-panel" class="uk-panel uk-panel-header">
+                    <h3 class="uk-panel-title">0ptions</h3>
+                    <i v-on:click="refresh()" class="uk-button uk-button-success uk-width-1-1">Refresh</i>
                 </div>
-                <div class="uk-width-large-5-6" data-uk-observe>
-                        <ul id="gallery" class="uk-grid uk-grid-match" data-uk-grid data-uk-grid-margin>
-                            <li class="uk-width-large-6-6">
-                                <i class="uk-icon-arrow-left uk-icon-large uk-icon-hover uk-width-2-6 uk-margin-small-bottom" style="text-align:right;" v-on:click="prevPage()" :disabled="page == 1"></i>
-                                <div class="uk-width-2-6 uk-text-center">
-                                    <h1 class="uk-text-center">{{activeChannel}}</h1>
-                                        <p>Page {{page}} out of {{pageCount}}</p>
-                                </div>
-                                <i class="uk-icon-arrow-right uk-icon-large uk-icon-hover uk-width-2-6 uk-margin-small-bottom" v-on:click="nextPage()" :disabled="page == pageCount"></i>
-                            </li>
-                            <li transition="fade" v-for="image in images" class="uk-width-large-1-6">
-                                <div class="uk-thumbnail gallery-image">
-                                    <a v-bind:href="image.url" style="display:block !important;margin:auto !important;">
-                                        <image-loader  v-bind:src="image.url"></image-loader>
-                                    </a>
-                                </div>
-                            </li>
-                        </ul>
-                </div>
+            </div>
+            <div class="uk-width-large-5-6" data-uk-observe>
+                    <ul id="gallery" class="uk-grid uk-grid-match" data-uk-grid data-uk-grid-margin>
+                        <li class="uk-width-large-6-6">
+                            <i class="uk-icon-arrow-left uk-icon-large uk-icon-hover uk-width-2-6 uk-margin-small-bottom" style="text-align:right;" v-on:click="prevPage()" :disabled="page == 1"></i>
+                            <div class="uk-width-2-6 uk-text-center">
+                                <h1 class="uk-text-center">{{activeChannel}}</h1>
+                                    <p>Page {{page}} out of {{pageCount}}</p>
+                            </div>
+                            <i class="uk-icon-arrow-right uk-icon-large uk-icon-hover uk-width-2-6 uk-margin-small-bottom" v-on:click="nextPage()" :disabled="page == pageCount"></i>
+                        </li>
+                        <li v-for="image in images" class="uk-width-large-1-6">
+                            <div class="uk-thumbnail gallery-image">
+                                <a href="#" v-on:click="displayImage(image.url)" style="display:block !important;margin:auto !important;">
+                                    <image-loader  v-bind:src="image.url"></image-loader>
+                                </a>
+                            </div>
+                        </li>
+                    </ul>
             </div>
         </div>
 
     </div>
 </template>
 <style>
+    .gallery-close-alt {
+        background:black;
+    }
     .gallery-container {
-        margin: 10px;
+        margin: 20px 10px;
     }
     .uk-thumbnail {
         background:rgba(0,0,0,0.8);
@@ -51,14 +59,6 @@
         padding:10px;
 
     }
-    .left-button {
-        border-top-right-radius: 0;
-        border-bottom-right-radius: 0;
-    }
-    .right-button {
-        border-top-left-radius: 0;
-        border-bottom-left-radius: 0;
-    }
 </style>
 <script>
     export default{
@@ -68,19 +68,35 @@
                 rowCount: 0,
                 pageCount: 0,
                 page: 1,
-                pageSize: 25,
+                pageSize: 12,
                 activeChannel: '#FSociety',
                 channels: [],
+                activeImage: '',
+                scrollToTop:0,
+
             };
         },
         components: {
         },
         ready(){
-            this.fetchImages();
+            $('footer').detach(); // Remove the footer
+            this.fetchImages(); // Fetch the images
+            this.initModal(); // Init the modal
         },
         methods: {
+            initModal: function() {
+                let modal = $('#image-modal');
+                window.UIkit.modal(modal, {}).on({
+                    'show.uk.modal': function(){
+                        this.scrollToTop = document.documentElement.scrollTop || document.body.scrollTop;
+                    },
+                    'hide.uk.modal': function(){
+                        document.documentElement.scrollTop = document.body.scrollTop = this.scrollToTop;
+                    }
+                });
+            },
             fetchImages: function (page) {
-                page = page || 1;
+                page = page || this.page;
                 this.$http.get('https://bot.fsociety.guru/api/urls', {
                     params: {
                         type: 'images',
@@ -101,6 +117,9 @@
                     });
                 });
             },
+            refresh: function() {
+              this.fetchImages(1);
+            },
             nextPage: function() {
                 if(this.page != this.pageCount) {
                     this.fetchImages(this.page + 1);
@@ -110,7 +129,17 @@
               if(this.page > 1) {
                   this.fetchImages(this.page - 1);
               }
-            }
+            },
+            displayImage: function(url) {
+                let uikit = window.UIkit;
+                let modal = uikit.modal('#image-modal');
+                this.activeImage = url;
+                if ( modal.isActive() ) {
+                    modal.hide();
+                } else {
+                    modal.show();
+                }
+            },
         }
     }
 </script>
