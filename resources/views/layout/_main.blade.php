17865b8165846fdef7c7365561cb5b5bfb9e76ce
Dave Richer <dave@develops.io>
2016-09-30 20:50:52 -0400

    much meta

--- resources/views/layout/_main.blade.php
+++ resources/views/layout/_main.blade.php
@@ -1,12 +1,24 @@
 <!DOCTYPE html>
-<html lang="en-gb" dir="ltr" class="uk-height-1-1">
+<html lang="en-us" dir="ltr" class="uk-height-1-1">
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
     <meta name="csrf-token" content="{!! csrf_token() !!}"/>
     <meta name="csrf-param" content="_token"/>
+    <meta name="description" content="Community content driven site made in homage to the critically acclaimed series Mr. Robot."/>
+    <meta name="robots" content="noindex,follow,noodp"/>
+    <meta property="og:locale" content="en_US" />
+    <meta property="og:type" content="object" />
+    <meta property="og:title" content="FSociety - Welcome" />
+    <meta property="og:description" content="Community content driven site made in homage to the critically acclaimed series Mr. Robot." />
+    <meta property="og:url" content="{{Request::url()}}" />
+    <meta property="og:site_name" content="FSociety" />
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta name="twitter:description" content="Community content driven site made in homage to the critically acclaimed series Mr. Robot." />
+    <meta name="twitter:title" content="FSociety" />
+    <meta name="twitter:site" content="@funsocietyirc" />
+
     <script src="{{elixir('js/app.js')}}"></script>
     <link rel="stylesheet" href={{elixir('css/uikit.css')}}>
     <link rel="stylesheet" href={{elixir('css/app.css')}}>
