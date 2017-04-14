66a8935f42a0fec333adb773df2b704e51df3844
Dave Richer <dave@develops.io>
2017-04-14 03:26:14 -0400

    finalize notify

--- resources/assets/js/components/watchYoutube.vue
+++ resources/assets/js/components/watchYoutube.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
-        <iframe class="fullscreen" v-if="currentKey" id="video" :src="currentUrl" frameborder="0" allowfullscreen></iframe>
+        <iframe class="fullscreen" v-if="currentKey" id="video" :src="currentUrl" frameborder="0"
+                allowfullscreen></iframe>
     </div>
 </template>
 <style>
@@ -15,34 +16,19 @@ body, html {
     top: 0;
     left: 0;
 }
+
 </style>
 <script>
     export default{
         data(){
             return{
                 currentKey: '',
-                currentTitle: '',
-                from: '',
-                to: '',
             }
         },
         computed: {
             currentUrl: function(){
                 if(!this.currentKey) return false;
                 return `//www.youtube.com/embed/${this.currentKey}?rel=0&autoplay=1`;
-            }
-        },
-        watch: {
-            currentTitle: function(newTitle) {
-                document.title = `${newTitle} - Powered by MrNodeBot`;
-            },
-            from: function(newFrom) {
-                UIkit.notify({
-                    message : `<div class="uk-text-center"><h4>Playing ${this.currentTitle}</h4><p>Requested By ${this.from} on ${this.to}</p></div>`,
-                    status  : 'info',
-                    timeout : 4000,
-                    pos     : 'bottom-center'
-                });
             }
         },
         mounted(){
@@ -52,13 +38,25 @@ body, html {
             initPusher: function () {
                 var self = this;
                 window.Fsociety.publicChannel.bind('youtube', data  => {
-                  if(self.currentKey === data.youtubeKey) self.currentKey = '';
-                  self.currentKey = data.youtubeKey;
-                  self.currentTitle = data.videoTitle;
-                  self.from = data.from;
-                  self.to = data.to;
+
+                    // Update the key, if they video is the same, then set the current frame to nothing for a sec
+                    if(self.currentKey === data.youtubeKey) self.currentKey = '';
+                    self.currentKey = data.youtubeKey;
+
+                    // Update title
+                    document.title = `${data.videoTitle} - Powered by MrNodeBot`;
+
+                    // Notify
+                    UIkit.notify({
+                        message : `<div class="uk-text-center"><h4>Playing ${data.videoTitle}</h4><p>Requested By ${data.from} on ${data.to}</p></div>`,
+                        status  : 'info',
+                        timeout : 4000,
+                        pos     : 'bottom-center'
+                    });
+
                 });
             }
         }
     }
+
 </script>
